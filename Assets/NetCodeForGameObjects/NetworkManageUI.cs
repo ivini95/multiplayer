b77c3849093a2429a69f3c709ab740f45446cb52
ivini95 <viniximenesdev@gmail.com>
2024-11-27 15:00:04 -0300

    first commit

---
+++ Assets/NetCodeForGameObjects/NetworkManageUI.cs
@@ -0,0 +1,23 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManageUI : MonoBehaviour
+{
+    [SerializeField] private Button ServerBtn;
+    [SerializeField] private Button HostBtn;
+    [SerializeField] private Button ClientBtn;
+
+    private void Awake(){
+        ServerBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartServer();
+        });
+        HostBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartHost();
+        });
+        ClientBtn.onClick.AddListener(() => {
+            NetworkManager.Singleton.StartClient();
+        });
+    }
+    
+}
